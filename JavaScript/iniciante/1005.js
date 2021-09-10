be7f372214341b68c1a8a542e8880085370673a1
luisa <luisa.ferreira@ufv.br>
2021-09-10 10:15:25 -0300

    1005

---
+++ JavaScript/iniciante/1005.js
@@ -0,0 +1,10 @@
+//var input = require('fs').readFileSync('/dev/stdin', 'utf8'); 
+var input = require('fs').readFileSync('stdin', 'utf8'); //para funcionar nesse computador 
+var lines = input.split('\n');
+
+var A = parseFloat(lines.shift());
+var B = parseFloat(lines.shift());
+
+var media = ((A*3.5)+(B*7.5))/11;
+
+console.log(`MEDIA = ${media.toFixed(5)}`);
