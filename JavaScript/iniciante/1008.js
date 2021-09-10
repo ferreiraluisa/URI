1fcffb880318f6fabdc9c23dc773d75b15b5f345
luisa <luisa.ferreira@ufv.br>
2021-09-10 10:37:30 -0300

    ajustando

---
+++ JavaScript/iniciante/1008.js
@@ -0,0 +1,11 @@
+//var input = require('fs').readFileSync('/dev/stdin', 'utf8'); 
+var input = require('fs').readFileSync('stdin', 'utf8'); //para funcionar nesse computador 
+var lines = input.split('\n');
+
+var number = parseInt(lines.shift());
+var numberWorkedHours = parseInt(lines.shift());
+var salaryPerHour = parseFloat(lines.shift());
+
+var salary = numberWorkedHours * salaryPerHour;
+
+console.log(`NUMBER = ${number} \nSALARY = U$ ${salary.toFixed(2)}`);
