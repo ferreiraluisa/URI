5d142fa77eb63743358a3fa8238072ff4bfac7e4
luisa <luisa.ferreira@ufv.br>
2021-09-10 12:22:55 -0300

    1008 - Salario com Bonus

---
+++ JavaScript/iniciante/1009.js
@@ -0,0 +1,11 @@
+//var input = require('fs').readFileSync('/dev/stdin', 'utf8'); 
+var input = require('fs').readFileSync('stdin', 'utf8'); //para funcionar nesse computador 
+var lines = input.split('\n');
+
+var name = lines.shift();
+var salary = parseFloat(lines.shift());
+var sells = parseFloat(lines.shift());
+
+var totalSalary = salary + (0.15 * sells);
+
+console.log(`TOTAL = R$ ${totalSalary.toFixed(2)}`);
