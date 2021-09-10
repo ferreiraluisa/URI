5c5daa4b345ca27d421b95ef05e07565eb6dca70
luisa <luisa.ferreira@ufv.br>
2021-09-10 09:48:38 -0300

    1003

---
+++ JavaScript/iniciante/1004.js
@@ -0,0 +1,8 @@
+//var input = require('fs').readFileSync('/dev/stdin', 'utf8'); 
+var input = require('fs').readFileSync('stdin', 'utf8'); //para funcionar nesse computador 
+var lines = input.split('\n');
+
+var A = parseInt(lines.shift());
+var B = parseInt(lines.shift());
+
+console.log(`PRODUTO = ${A*B}`);
