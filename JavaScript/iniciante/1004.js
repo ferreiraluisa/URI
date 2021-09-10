8ca582a1852a85a4138bf09a402c2e194090a5fa
luisa <luisa.ferreira@ufv.br>
2021-09-10 09:53:02 -0300

    1004

--- JavaScript/iniciante/1004.js
+++ JavaScript/iniciante/1004.js
@@ -1,8 +1,8 @@
-//var input = require('fs').readFileSync('/dev/stdin', 'utf8'); 
-var input = require('fs').readFileSync('stdin', 'utf8'); //para funcionar nesse computador 
+var input = require('fs').readFileSync('/dev/stdin', 'utf8'); 
+//var input = require('fs').readFileSync('stdin', 'utf8'); //para funcionar nesse computador 
 var lines = input.split('\n');
 
 var A = parseInt(lines.shift());
 var B = parseInt(lines.shift());
 
-console.log(`PRODUTO = ${A*B}`);
+console.log(`PROD = ${A*B}`);
