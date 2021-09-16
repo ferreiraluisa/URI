31b15c099859ad91d2e348159d843f236f78faf9
luisa <02luisafsouza@gmail.com>
2021-09-16 16:07:48 -0300

    1017 - Gasto em combustivel

---
+++ JavaScript/iniciante/1017.js
@@ -0,0 +1,13 @@
+/*Joaozinho quer calcular e mostrar a quantidade de litros de combustível gastos em uma viagem, ao utilizar um automóvel que faz 12 KM/L. Para isso, ele gostaria que você o auxiliasse através de um simples programa. Para efetuar o cálculo, deve-se fornecer o tempo gasto na viagem (em horas) e a velocidade média durante a mesma (em km/h). Assim, pode-se obter distância percorrida e, em seguida, calcular quantos litros seriam necessários. Mostre o valor com 3 casas decimais após o ponto. */
+var input = require('fs').readFileSync('/dev/stdin', 'utf8');
+//var input = require('fs').readFileSync('stdin', 'utf8');
+var lines = input.split('\n');
+
+var tempoGasto = lines.shift();
+var velMedia = lines.shift();
+
+var distancia = tempoGasto * velMedia;
+
+var litrosNecessarios = distancia/12;
+
+console.log(litrosNecessarios.toFixed(3));
